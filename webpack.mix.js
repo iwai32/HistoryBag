1216ef310b276adeb08b8b9a8205ea24eca4d446
iwai32 <iwakou32@gmail.com>
2020-01-22 20:43:31 +0900

    laravelMixの設定

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,13 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .version();
+
+mix.browserSync({
+    files: [
+        "resources/views/**/*.blade.php",
+        "public/**/*.*"
+    ],
+    proxy: "http://historybag.test/"
+});
