37c32a39afc3d79b479dd9ba43f1caf223dc031a
iwai32 <iwakou32@gmail.com>
2020-01-23 17:42:42 +0900

    グローバルヘッダーのレイアウト作成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,32 +25,42 @@
 <body>
   <!--header-->
   <header class="g-header">
+    <div class="user-wrapper">
+      <div class="user-guide">
+        <p class="user-guide__to-top"><a class="user-guide__to-top__anchor" href="#">top</a></p>
+        <div class="user-guide__groups">
+          <ul class="user-guide__account">
+            <li class="user-guide__account__link">
+              <a class="user-guide__account__link__anchor" href="#">新規登録</a>
+            </li>
+            <li class="user-guide__account__link">
+              <a class="user-guide__account__link__anchor" href="#">ログイン</a>
+            </li>
+          </ul>
+          <button class="user-guide__language">Language</button>
+        </div>
+      </div>
 
-    <div class="g-nav g-nav--sub">
-      <p class="g-nav--sub__to-top"><a class="g-nav--sub__to-top__anchor" href="#">top</a></p>
-      <div class="g-nav--sub__groups">
-        <ul class="g-nav--sub__account">
-          <li class="g-nav--sub__account__link">
-            <a class="g-nav--sub__account__link__anchor" href="#">新規登録</a>
-          </li>
-          <li class="g-nav--sub__account__link">
-            <a class="g-nav--sub__account__link__anchor" href="#">ログイン</a>
-          </li>
-        </ul>
-        <button class="g-nav--sub__language">Language</button>
+      <div class="user-contents">
+        <h1 class="app-title">HistoryBag</h1>
+        <div class="user-contents__nav">
+          <ul class="user-contents__nav__items">
+            <li class="account-nav"><i class="fas fa-user-circle"></i></li>
+            <li class="favorites"><i class="fas fa-star"></i></li>
+            <li class="logout">ログアウト</li>
+          </ul>
+        </div>
       </div>
     </div>
 
-    <div class="inner g-header-inner">
-      <h1 class="app-title">HistoryBag</h1>
-      <div class="g-nav g-nav--main">
-        <ul class="g-nav--main__items">
-          <li class="account-nav"><i class="far fa-user-circle"></i></li>
-          <li class="favorites">お気に入り</li>
-          <li class="logout">ログアウト</li>
-        </ul>
-      </div>
-    </div>
+    <nav class="g-nav">
+      <ul>
+        <li class="g-nav__list current"><a href="#">日報</a></li>
+        <li class="g-nav__list"><a href="#">スニペット</a></li>
+        <li class="g-nav__list"><a href="#">チャートグラフ</a></li>
+        <li class="g-nav__list"><a href="#">メモ</a></li>
+      </ul>
+    </nav>
 
   </header>
 
