e0d3f91f35962f8b9186b3ad257203db9bd3bd70
iwai32 <iwakou32@gmail.com>
2020-02-16 04:52:36 +0900

    OAuth(google)認証の機能を作成

---
+++ app/Http/Controllers/User/OauthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Auth;
+use Socialite;
+
+class OauthController extends Controller
+{
+  private $user;
+
+  public function __construct(User $user)
+  {
+    $this->user = $user;
+  }
+
+  /**
+   * Googleの認証ページへユーザーをリダイレクト
+   */
+    public function redirectToGoogle()
+    {
+      return Socialite::driver('google')->redirect();
+    }
+
+    /**
+     * Googleからユーザー情報を取得
+     */
+    public function authGoogleCallback()
+    {
+      $googleUser =  Socialite::driver('google')->user();
+
+      $userInputs = [
+        'email' => $googleUser->email,
+        'name' => $googleUser->nickname ?? $googleUser->name,
+      ];
+
+      return $this->authAndSignin($userInputs);
+    }
+
+    /**
+     * 認証情報を元にサインインする
+     */
+    public function authAndSignin($userInputs)
+    {
+      $user = $this->user->firstOrNew([
+        'email' => $userInputs['email']
+      ],[
+        'name' => $userInputs['name'],
+        'password' => 'socialuser'
+      ]);
+
+      $user->save();
+
+      Auth::login($user);
+
+      return redirect()->route('dailyReport');
+    }
+}
