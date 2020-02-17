6e4b2d2984d3a093abe45f802b058371e0160954
iwai32 <iwakou32@gmail.com>
2020-02-18 02:03:02 +0900

    検索UIをコンポーネント化

---
+++ resources/views/contents/components/globalSearcher.blade.php
@@ -0,0 +1,59 @@
+<div class="g-searcher">
+  <form action="#" method="get">
+      <div class="inner g-searcher-inner">
+      <!-- ページ検索 -->
+      <div class="search-item g-search-page">
+        <select class="g-search-page__type" name="page-type">
+          <option value="ALL" selected="selected">すべて</option>
+          <option value="日報">日報</option>
+          <option value="スニペット">スニペット</option>
+          <option value="メモ">メモ</option>
+        </select>
+      </div>
+      <!-- 日付検索 -->
+      <div class="search-item g-search-date">
+        <input class="g-search-date__input" type="date" name="date">
+      </div>
+      <!-- キーワード検索 -->
+      <div class="search-item g-search-keyword">
+        <input class="g-search-keyword__input" type="search" name="keyword">
+        <button class="g-search-keyword__button" type="button"><i class="fas fa-search"></i></button>
+      </div>
+      <!-- カテゴリ検索 -->
+      <div class="search-item g-search-categories">
+        <label class="g-search-categories__label">
+          <input class="g-search-categories__box" type="checkbox" name="category" value="HTML">
+          <span>HTML</span>
+            </label>
+        <label class="g-search-categories__label">
+          <input class="g-search-categories__box" type="checkbox" name="category" value="CSS">
+          <span>CSS</span>
+        </label>
+        <label class="g-search-categories__label">
+          <input class="g-search-categories__box" type="checkbox" name="category" value="JavaScript">
+          <span>JavaScript</span>
+        </label>
+        <label class="g-search-categories__label">
+          <input class="g-search-categories__box" type="checkbox" name="category" value="PHP">
+          <span>PHP</span>
+        </label>
+        <label class="g-search-categories__label">
+          <input class="g-search-categories__box" type="checkbox" name="category" value="SQL">
+          <span>SQL</span>
+        </label>
+        <label class="g-search-categories__label">
+          <input class="g-search-categories__box" type="checkbox" name="category" value="AWS">
+          <span>AWS</span>
+        </label>
+        <label class="g-search-categories__label">
+          <input class="g-search-categories__box" type="checkbox" name="category" value="Linux">
+          <span>Linux</span>
+        </label>
+        <label class="g-search-categories__label">
+          <input class="g-search-categories__box" type="checkbox" name="category" value="その他">
+          <span>その他</span>
+        </label>
+      </div>
+    </div>
+  </form>
+</div>
