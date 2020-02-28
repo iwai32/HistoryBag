dec572300a3a40c92b69e429fa0618c6a2e911ce
iwai32 <iwakou32@gmail.com>
2020-02-28 16:48:50 +0900

    日報の画面作成

--- resources/views/contents/components/definitionList.blade.php
+++ resources/views/contents/components/definitionList.blade.php
@@ -1,10 +1,13 @@
 <dl class="contents-definition-list">
-  <dt>タイトル</dt>
+  <dt>タイトル<span class="required">*</span></dt>
   <dd>{{ $titleContent }}</dd>
 
   <dt>カテゴリ</dt>
   <dd>{{ $categoryContent }}</dd>
 
-  <dt>日付</dt>
+  <dt>日付<span class="required">*</span></dt>
   <dd>{{ $dateContent }}</dd>
+
+  <dt>サムネイル</dt>
+  <dd>{{ $photoContent }}</dd>
 </dl>
